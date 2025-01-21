d64450e65a7a800ddfe0ee901b6dad11f6a75df2
Dhruv <dshivhare661@gmail.com>
2025-01-21 12:24:12 +0530

    provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function App() {
+
   return (
     <div>App</div>
   )
---
+++ src/Store.jsx
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterRd from './Createslice'
+const store = configureStore({
+    reducer:{
+        counter:counterRd
+
+    }
+})
+
+export default store
